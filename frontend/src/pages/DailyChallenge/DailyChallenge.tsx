7be38f2076fc161423d74482fb82cfa7ddf11b8b
hass <-@>
2026-04-18 02:37:11 +0300

    base mobile css

--- frontend/src/pages/DailyChallenge/DailyChallenge.tsx
+++ frontend/src/pages/DailyChallenge/DailyChallenge.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { dailyApi, authApi } from '@/services/api';
-import { ResultModal } from '@/components/ResultModal.tsx';
+import { ResultModal } from '@/components/ResultModal';
 import './DailyChallenge.css';
 import {
   hasCompletedToday,
